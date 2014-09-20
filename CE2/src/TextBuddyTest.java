f5abc86fbf131fe254590efdb0a6fdbdfadcc45c
radityapujamurti <radityapujamurti@gmail.com>
2014-09-20 21:59:28 +0800

    updated the test expected

--- CE2/src/TextBuddyTest.java
+++ CE2/src/TextBuddyTest.java
@@ -1,25 +1,22 @@
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
-
 import org.junit.Test;
 
 public class TextBuddyTest {
-	@Test 
+	@Test
 	
 	public void testProcessCommand() throws IOException{
-		
-		testInitialize("initialize the file","CE2.txt created!","CE2.txt");
-		//testOneCommand("simple display before any add","CE2.txt is empty","display CE2.txt");
-		//testOneCommand("simple add to file","added to CE2.txt: test 1","add test1");
-		
-		//testThreeCommand("simple add","added to CE2.txt: \"test1\"", "test1");
-		//testTwoCommand("clear file before any input","null is cleared!", "CE2.txt");
-	}
-	
-	private void testInitialize(String description, String expected, String fileName) throws IOException {
-		// TODO Auto-generated method stub
-		assertEquals(description, expected, textBuddy.initialize(fileName));
+		testOneCommand("unsuccesful search: search empty file", "CE2.txt is empty","search test");
+		testOneCommand("unsuccesful sort: sorting empty file","CE2.txt is empty!","sort");
+		testOneCommand("simple display before any add","CE2.txt is empty","display CE2.txt");
+		testOneCommand("simple add to file","added to CE2.txt: \"test 1\"","add test 1");
+		testOneCommand("simple add to file","added to CE2.txt: \"test 2\"","add test 2");
+		testOneCommand("simple add to file","added to CE2.txt: \"random\"","add random");
+		testOneCommand("successful search file","Found!","search test");
+		testOneCommand("unsuccessful search file","Not found!","search simple");
+		testOneCommand("successful sort","CE2.txt is is sorted alphabetically!","sort");
+			
 	}
 
 	private void testOneCommand(String description, String expected, String command) throws IOException {
@@ -27,13 +24,4 @@ public class TextBuddyTest {
 		assertEquals(description, expected, textBuddy.processCommand(command));
 	}
 	
-	private void testTwoCommand(String description, String expected, String fileName) throws IOException {
-		// TODO Auto-generated method stub
-		assertEquals(description, expected, textBuddy.clearFile(fileName));
-	}
-	
-	private void testThreeCommand(String description, String expected, String toBeAdded) throws IOException {
-		// TODO Auto-generated method stub
-		assertEquals(description, expected, textBuddy.addToFile(toBeAdded));
-	}
 }
